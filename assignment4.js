44f6a87a6cd8674803fd2f57e45dc11ff632ad27
Ayush Tiwari <atiwari@grepruby.com>
2017-06-14 00:50:13 +0530

    changed indentation and made other changes

--- assignment4.js
+++ assignment4.js
@@ -1,17 +1,16 @@
 let files = ["./f1","./f2","./f3", "./f4", "./f5"], i = 0;
 fs = require('fs');
-function callBackExample(file){
-  fs.readFile(file[i], 'utf8' , function (err,data) {
+function fileReader(file){
+  fs.readFile(fileList[i], 'utf8' , function (err,data) {
     if (err) {
-	  return console.log(err);
-	}
-	console.log(data);
-	console.log('==================');
-	i++;
-	if(i<5) {
-	  callBackExample(file);
-	}
+    return console.log(err);
+  }
+  console.log(data);
+  console.log('==================');
+  i++;
+  if(i < 5) {
+    callBackExample(fileList);
+  }
   });
-
 }
-callBackExample(files);
+fileReader(files);
