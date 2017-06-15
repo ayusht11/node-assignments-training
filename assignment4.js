c95e2ea659068126bc112ae50199e9072180606b
Ayush Tiwari <atiwari@grepruby.com>
2017-06-15 19:32:06 +0530

    improved readability

--- assignment4.js
+++ assignment4.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
+
 const files = ["./f1","./f2","./f3", "./f4", "./f5"];
 let fileNo = 0;
+
 function fileReader(fileList) {
-  fs.readFile(fileList[fileNo], 'utf8', function (err,data) {
+  fs.readFile(fileList[fileNo], 'utf8', function (err, data) {
     if (err) {
       return console.log(err);
     }
     console.log(data);
     console.log('==================');
     fileNo++;
-    if(fileNo < 5) {
+    if (fileNo < 5) {
       fileReader(fileList);
     }
   });
