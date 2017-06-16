eb4639db039aa1364bc723defcad331a7c8b0025
Ayush Tiwari <atiwari@grepruby.com>
2017-06-16 15:17:03 +0530

    improved function name and indentation edits

--- assignment5/assignment5.js
+++ assignment5/assignment5.js
@@ -1,24 +1,25 @@
 const fs = require('fs');
 
-function reader(file) {
+function readingFile(file) {
   return new Promise(function (resolve, reject) {
     fs.readFile(file, 'utf8' , function (err, data) {
       if (err) {
-      reject(err);
-    }
-    resolve(data);
-    console.log("===========================");
+        reject(err);
+      }
+      resolve(data);
+      console.log("===========================");
     })
   });
 }
 
-reader("./f1").then(function (result) {
-  console.log(result);
-  return reader("./f2");
+readingFile("./f1")
+ .then(function (result) {
+    console.log(result);
+    return readingFile("./f2");
 })
  .then(function(result) {
     console.log(result);
-    return reader("./f3");
+    return readingFile("./f3");
 })
  .then(function(result) {
     console.log(result);
