9a453765eaa3d87cbc635bde354b680798282677
Ayush Tiwari <atiwari@grepruby.com>
2017-06-19 12:59:35 +0530

    assignment 6 - Module 2: 3rd Party API Integration Assignment

--- myapp/app.js
+++ myapp/app.js
@@ -1,68 +1,16 @@
 const express = require('express');
-const request = require('request');
-const bodyParser = require('body-parser');
+const app = module.exports = express();
 
-const app = express();
-let jsonParser = bodyParser.json();
+const getClients = require('./getClients.js'); 
+const getClientInfo = require('./getClientInfo.js');
+const getClientLists = require('./getClientLists.js');
+const createClient = require('./createClient.js');
+const deleteClient = require('./deleteClient.js');
+const listenPort = require('./listenPort.js'); 
 
-app.get('/clients', function (req, res) {
-  request('https://api.createsend.com/api/v3.1/clients.json', function (err, response, data) {
-    if(err) {
-      res.send(err);
-    }
-    res.json(data); 
-  })
-   .auth('fc96dc9ded698bbfbfb975409a864a42', 'ayusht11', false);
-});
-
-app.get('/clients/:clientid', function (req, res) {
-  request('https://api.createsend.com/api/v3.1/clients/'+req.params.clientid+'.json', function (err, response, data) {
-    if(err) {
-      res.send(err);
-    }
-    res.json(data); 
-  })
-   .auth('fc96dc9ded698bbfbfb975409a864a42', 'ayusht11', false);
-});
-
-app.get('/clients/:clientid/lists', function (req, res) {
-  request('https://api.createsend.com/api/v3.1/clients/'+req.params.clientid+'/lists.json', function (err, response, data) {
-    if(err) {
-      res.send(err);
-    }
-    res.json(data); 
-  })
-   .auth('fc96dc9ded698bbfbfb975409a864a42', 'ayusht11', false);
-});
-
-app.post('/clients', jsonParser, function (req, res) {
-  request({
-    url: 'https://api.createsend.com/api/v3.1/clients.json',
-    method: "POST",
-    json: req.body
-    },
-    function (err, response, data) {
-      if(err) {
-        res.send(err);
-      }
-      res.json(data);
-    })
-     .auth('fc96dc9ded698bbfbfb975409a864a42', 'ayusht11', true);
-});
-
-app.delete('/clients/:client', function (req, res) {
-  request({
-    url: 'https://api.createsend.com/api/v3.1/clients/'+req.params.client+'.json',
-    method: "DELETE",
-    },
-    function (err, response, data) {
-      if(err)
-        {res.send(err);}
-      res.json(data);
-    })
-     .auth('fc96dc9ded698bbfbfb975409a864a42', 'ayusht11', false);
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-});
+getClients();
+getClientInfo();
+getClientLists();
+createClient();
+deleteClient();
+listenPort();
